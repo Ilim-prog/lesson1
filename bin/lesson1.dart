05914a3f4031f1acc91a752381071e5e1cf5636e
Ilim-prog <ilimbek.baturbek.01@gmail.com>
2022-06-28 20:32:02 +0600

    first commit

--- bin/lesson1.dart
+++ bin/lesson1.dart
@@ -1,5 +1,106 @@
 import 'package:lesson1/lesson1.dart' as lesson1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lesson1.calculate()}!');
+
+  num i = 12;
+  num m = 23.1;
+  num l = 16;
+
+  int d1 = 12;
+  int b1 = 16;
+  int h1 = 10;
+   
+  double e1 = 12;
+  double j1 = 9.7;
+  double k2 = 7.4;
+
+  String name ='ilim';
+  String name1 = "ilim";
+  String name2 = 'izat';
+  String name3 = "izat";
+
+  List array = [1,2,4,5,true,false, 'asd'];
+  List array1 = [1,2,3,false,true, 'ads'];
+  List array2 = [1,2,3,4,true,false,'dsa'];
+  
+
+  Map myMap = {'name':'ilim','age':'21', 'isStudent':true};
+  Map myMap1 ={'name':'izat','age':'20','isStudent':false};
+  Map Student = {'name':'beka','age':'21','Branch':true};
+
+  const ad = 1;
+  const sa = 2;
+  const da = 3;
+
+
+  final ac;
+  ac = 10;
+  final ah;
+  ah = 14;
+  final aj;
+  aj = 12;
+
+  var add1 = 'afddsf';
+  add1 = 'vdfvfd';
+  add1 = 'cjsdbjv';
+
+  var ass2 = 'wqe';
+  ass2 = 'sdjkffjs'; 
+  ass2 = 'dhshkkjv';
+
+  var ahh = 'cds';
+  ahh = 'fhfhd';
+  ahh = 'dsfssd';
+
+  dynamic number = 17;
+  number = 13;
+  number = false;
+  number = 'qwe';
+  number = [];
+
+  dynamic number1 = 10;
+  number1= 12;
+  number1 = true;
+  number1 = 'qwe';
+  number1 = {};
+
+  dynamic number2 = 20;
+  number2 = 14;
+  number2 = true;
+  number2 = 'wer';
+  number2 = ();
+
+  
+//   num a = 10;
+//   num b = 24.4;
+
+//   int a1 = 10;
+//   double b1 = 14.4;
+
+//   String name = 'dastan';
+//   String name2 = "dastan";
+
+//   List array = [1,2,4,true, false, 'qwe'];
+
+//   Map myMap = {'name': 'dastan', 'age': 10, 'isStudent': true};
+
+//   print(myMap['isStudent']);
+
+//   const aa = 10;
+
+// final ad ;
+// ad =10;
+
+
+
+//  var aa1 = 'qwe';
+//  aa1 = 'sadsad';
+//  aa1 = 'csafdsad';
+
+// dynamic ac = 10;
+// ac = 16;
+// ac = true;
+// ac = [];
+// ac ='qwe';
+
 }
